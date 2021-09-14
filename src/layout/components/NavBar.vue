60d0be667a32f31ba3ae0a34a3bd6bd1f0927341
suwei <910933955@qq.com>
2021-09-14 17:34:09 +0800

    feat: firstCommit

---
+++ src/layout/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="common-header">
+    <div class="left-content">
+      <!-- <img src="@/assets/images/logo.png" alt="LOGO" width="100" /> -->
+      <span class="logo">LOGO</span>
+      <div v-if="productName || companyName" class="page-title">
+        <span v-if="productName">{{ productName }}</span>
+        <span v-if="productName && companyName"> - </span>
+        <span v-if="companyName">{{ companyName }}</span>
+      </div>
+    </div>
+    <div class="center-content">
+      <slot name="centerContent"></slot>
+    </div>
+    <el-popover placement="bottom">
+      <div class="logout-pop" style="cursor: pointer">退出</div>
+      <div class="right-content" slot="reference">
+        <div class="user-name">名字名字名字名字</div>
+        <img :src="arrow_down" class="arrow-down" />
+      </div>
+    </el-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonHeader",
+  props: {
+    productName: String,
+    companyName: String
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+.common-header {
+  width: 100%;
+  height: 60px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  font-size: 14px;
+  background-color: #0780ed;
+  color: #fff;
+}
+.left-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.right-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.page-title {
+  margin-left: 21px;
+  position: relative;
+}
+.user-name {
+  margin: 0 5px 0 10px;
+  max-width: 72px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav-bar productName="suwei" companyName="suwei" />
+    <div class="main-container">
+      <sidebar class="sidebar-container" />
+      <div class="right-container">
+        <div class="bread-container">
+          <breadcrumb id="breadcrumb-container" class="breadcrumb-container" />
+        </div>
+        <app-main />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavBar, Sidebar, AppMain } from "./components";
+import Breadcrumb from "@/components/Breadcrumb";
+
+export default {
+  name: "Layout",
+  components: {
+    NavBar,
+    Sidebar,
+    AppMain,
+    Breadcrumb
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.right-container {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  margin: 24px;
+  background: #f4f5f9;
+}
+
+.bread-container {
+  box-sizing: border-box;
+  background: #fff;
+  margin-bottom: 14px;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+}
+</style>
