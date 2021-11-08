d5971a71f0560257ee63afde1bd593dd1db38592
wuleip <wuleip@enn.cn>
2021-11-08 16:29:58 +0800

    add log for searching problem

--- src/permission.js
+++ src/permission.js
@@ -4,8 +4,13 @@ import store from "./store";
 
 router.beforeEach(async (to, from, next) => {
   document.title = getPageTitle(to.meta.title);
-  await store.dispatch("user/getInfo");
-  next();
+  try {
+    await store.dispatch("user/getInfo");
+    next();
+  } catch {
+    console.log("=============== router.beforeEach")
+    next();
+  }
   // const hasPreventId =
   //   store.getters.preventId && store.getters.preventId.length > 0;
 
