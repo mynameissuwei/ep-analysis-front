c58bda545f4014ab08c881490a4d34b379377c27
wuleip <wuleip@enn.cn>
2021-11-18 16:19:21 +0800

    修改接入apm监控

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import App from "./App";
 import store from "./store";
 import router from "./router";
 
+
 import "@/icons"; // icon
 import "@/permission"; // permission control
 import * as Echarts from "echarts";
@@ -60,6 +61,23 @@ function initVue() {
   Vue.use(ElementUI);
 
   Vue.config.productionTip = false;
+
+  new MonitorJS().init({
+    pageId: 'ep-analysis-front', //上线应用id
+    env: MonitorJS.EnnDEV, //上报错误地址
+    consoleError:true,
+    vueError:true, //是否上报Vue错误
+    vue:Vue,
+
+  });
+
+  new MonitorJS().monitorPerformance({
+    pageId: 'ep-analysis-front',
+    env: MonitorJS.EnnDEV, //上报错误地址
+    isRXHR: false,
+    isPage: true,
+
+  });
 
   new Vue({
     el: "#app",
