9ae1ca93de7b83945874f80a211117f1d426036b
suwei <suwei@suweideMacBook-Pro.local>
2021-12-20 13:46:24 +0800

    fix: 417bugFix

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,49 +9,49 @@ const service = axios.create({
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 10000, // request timeout
   headers: {
-    'x-gw-accessKey': process.env.VUE_APP_ACCESS_KEY
-  }
+    "x-gw-accessKey": process.env.VUE_APP_ACCESS_KEY,
+  },
 });
 
 // request interceptor
 service.interceptors.request.use(
-  async config => {
+  async (config) => {
     if (config.method === "post" || config.method === "put") {
       const data = await store.dispatch("user/getPreventId");
       config.headers["token"] = data;
     }
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
 
 // response interceptor
 service.interceptors.response.use(
-  response => {
+  (response) => {
     const res = response.data;
     // if the custom code is not 0, it is judged as an error.
+
     if (res.code != 0 || res.code != "0") {
       Message({
         message: res.message || "系统繁忙，请稍后再试",
         type: "error",
-        duration: 5 * 1000
+        duration: 5 * 1000,
       });
-      store.dispatch('loading/cancelLoading')
+      store.dispatch("loading/cancelLoading");
       return Promise.reject(new Error(res.message || "Error"));
     } else {
       return res;
     }
   },
-  error => {
-    // for debug
+  (error) => {
+    // Message({
+    //   message: error.message,
+    //   type: "error",
+    //   duration: 5 * 1000,
+    // });
     console.log("err" + error); // for debug
-    Message({
-      message: error.message,
-      type: "error",
-      duration: 5 * 1000
-    });
     return Promise.reject(error);
   }
 );
