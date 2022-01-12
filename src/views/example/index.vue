9d0a206a4e082b289ae40d3e984fd6aa5f12ef73
‘yanjiaqiang’ <yanjiaqiang@enn.cn>
2022-01-12 13:46:35 +0800

    bpmn流程图

--- src/views/example/index.vue
+++ src/views/example/index.vue
@@ -138,7 +138,6 @@
                   :nodeTimeData="nodeTimeData"
                   :nodeChartData="nodeChartData"
                   :listQuery="listQuery"
-                  @selectNodes="selectNodes"
                 />
               </el-tab-pane>
               <el-tab-pane
@@ -214,6 +213,9 @@ export default {
     let $this = this;
     Bus.$on("selectNodes", (data) => {
       $this.pd.selectNodes(data);
+    });
+    Bus.$on("unSelectNodes", () => {
+      $this.pd.unSelectNodes();
     });
   },
   data() {
@@ -301,6 +303,9 @@ export default {
   methods: {
     selectNodes(ids) {
       this.pd.selectNodes(ids);
+    },
+    unSelectNodes(){
+      this.pd.unSelectNodes();
     },
     initPD() {
       this.pd = new PD("process_graph");
