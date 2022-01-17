4be7b70fe92332a459de367c418010645440f6f6
suwei <suwei@suweideMacBook-Pro.local>
2022-01-17 11:35:07 +0800

    fix: 更改标题 效能分析

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,10 @@
 import defaultSettings from "@/settings";
 
-const title = defaultSettings.title || "效能词典";
+const title = defaultSettings.title || "效能分析";
 
 export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`;
-  }
+  // if (pageTitle) {
+  //   return `${pageTitle} - ${title}`;
+  // }
   return `${title}`;
 }
